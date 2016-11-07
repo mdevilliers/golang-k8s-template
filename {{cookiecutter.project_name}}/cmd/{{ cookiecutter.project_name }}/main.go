1cf2d93005c82a46496e245004f927e6696dee4e
Mark deVilliers <markdevilliers@gmail.com>
2016-11-07 10:09:37 +0000

    add a webserver with hard coded response

--- {{cookiecutter.project_name}}/cmd/{{ cookiecutter.project_name }}/main.go
+++ {{cookiecutter.project_name}}/cmd/{{ cookiecutter.project_name }}/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,10 +11,25 @@ import (
 
 func main() {
 
-	log.Printf("{{ cookiecutter.project_name }} running...")
+	log.Printf("{{ cookiecutter.project_name }} starting...")
+
+	// start a server to respond on port 3000
+	go startHttpServer()
 
 	quit := make(chan os.Signal, 1)
+
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	log.Printf("Shutdown signal received, exiting...")
+}
+
+func startHttpServer() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "{{ cookiecutter.project_name }} says 'hello from K8s!'")
+	})
+
+	log.Printf("{{ cookiecutter.project_name }} running...")
+
+	http.ListenAndServe(":3000", nil)
 }
