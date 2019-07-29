a4fa7d4bf72520fccaeee311d30aab5e3d87e830
mdevilliers <markdevilliers@gmail.com>
2019-07-29 09:36:36 +0100

    backport latest changes

---
+++ {{cookiecutter.project_name}}/internal/logger/logger.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"io"
+	"os"
+
+	"github.com/mdevilliers/cache-service/internal/version"
+	"github.com/rs/zerolog"
+)
+
+// New returns an instntiated, configured logger
+// This really should be in a shared library!
+func New(logLevel string, useConsole, makeVerbose bool) zerolog.Logger {
+
+	var w io.Writer = os.Stdout
+	if useConsole {
+		w = zerolog.ConsoleWriter{
+			Out: os.Stdout,
+		}
+	}
+	// Parse level from config
+	lvl, err := zerolog.ParseLevel(logLevel)
+	if err != nil {
+		lvl = zerolog.InfoLevel
+	}
+	// Override level with verbose
+	if makeVerbose {
+		lvl = zerolog.DebugLevel
+	}
+	return zerolog.New(w).Level(lvl).With().Fields(map[string]interface{}{
+		"version": version.Version,
+		"app":     "cache-service",
+	}).Timestamp().Logger()
+}
